f54cfba57a80940649806a93579b00c221357f47
VictorWangwz <wangzhen5161@outlook.com>
2019-03-14 21:53:06 -0700

    dependencies

--- chat/consumers.py
+++ chat/consumers.py
@@ -29,21 +29,22 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-
+        exit = 0
+        if "exit" in text_data_json.keys():
+            exit = text_data_json["exit"]
+        print(exit)
         # Send message to room group
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                "message": message,
+                "exit":exit
             }
         )
 
     # Receive message from room group
     async def chat_message(self, event):
-        message = event['message']
 
         # Send message to WebSocket
-        await self.send(text_data=json.dumps({
-            'message': message
-        }))
+        await self.send(text_data=json.dumps(event))
